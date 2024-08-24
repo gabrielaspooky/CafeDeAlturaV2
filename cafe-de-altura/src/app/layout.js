8a44219befe5da8b1ccbbe3931cdeaf37a3af4b4
gabrielaspooky <gabrielanava01@gmail.com>
2024-08-24 12:20:46 +0200

    added config files

---
+++ cafe-de-altura/src/app/layout.js
@@ -0,0 +1,21 @@
+import { Outfit } from "next/font/google";
+import "./globals.css";
+// import Navbar from "../../components/ui/Navbar";
+
+const inter = Outfit({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Café de altura",
+  description: "Todos los derechos reservados - Café de Altura SL - 2024",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body className={inter.className}>
+        {/* <Navbar /> */}
+        {children}
+        </body>
+    </html>
+  );
+}
