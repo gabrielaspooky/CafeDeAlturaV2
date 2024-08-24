05bccd4566a6dc2a76f9e6c71306ba90d0c5085b
gabrielaspooky <gabrielanava01@gmail.com>
2024-08-24 15:44:16 +0200

    Connected to MongoDB successfully

--- cafe-de-altura/lib/mongodb.mjs
+++ cafe-de-altura/lib/mongodb.mjs
@@ -1,28 +1,29 @@
-import { MongoClient } from 'mongodb';
+// lib/mongodb.mjs
+import { MongoClient, ServerApiVersion } from 'mongodb';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config(); // Cargar variables de entorno desde .env
 
 const uri = process.env.MONGODB_URI;
-const dbName = process.env.DB_NAME;
-
-let client;
-let db;
-
 if (!uri) {
-  throw new Error('Please add your Mongo URI to .env.local');
+  throw new Error('Please add your Mongo URI to .env');
 }
 
-export async function connectToDatabase() {
-  if (db) {
-    return { db, client };
-  }
-  client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-  await client.connect();
-  db = client.db(dbName);
-  return { db, client };
+export async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB successfully!");
+    return client.db(); // Retorna la base de datos por defecto
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  }
 }
