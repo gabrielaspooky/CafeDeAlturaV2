f38cfe021b3a9d0afaf2b4afa74d51825c2d61a4
gabrielaspooky <gabrielanava01@gmail.com>
2024-08-26 09:58:18 +0200

    added fetching update

--- cafe-de-altura/src/app/api/products/route.js
+++ cafe-de-altura/src/app/api/products/route.js
@@ -1,14 +1,21 @@
 import { NextResponse } from 'next/server';
-import { dbConnect } from '../../../../lib/mongodb.mjs'; // Importa usando el nombre de la exportación
-import Product from '../../../../models/Product'; // Asegúrate de que esta ruta sea correcta
+import { dbConnect } from '../../../../lib/mongodb.mjs'; // Ruta a la conexión de MongoDB
+import Product from '../../../../models/Product'; // Ruta al modelo Product
 
 export async function GET(request) {
   try {
+    // Conectarse a la base de datos
     await dbConnect();
+
+    // Obtener los productos desde la base de datos
     const products = await Product.find().lean();
+
+    // Responder con la lista de productos
     return NextResponse.json({ products });
   } catch (error) {
     console.error('Error fetching products:', error);
+
+    // Responder con un error en caso de fallo
     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
   }
 }
